4d48d7f76c8c97f476b1960c827d11ea4ace6c97
Bo Seerden <101641491+bdseerden@users.noreply.github.com>
2024-07-15 10:09:41 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,9 @@
+module sendgrid_email_sender
+
+go 1.21.1
+
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/sendgrid/rest v2.6.9+incompatible // indirect
+	github.com/sendgrid/sendgrid-go v3.14.0+incompatible // indirect
+)
---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
+)
+
+func main() {
+	// Load environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	sendgridAPIKey := os.Getenv("SENDGRID_API_KEY")
+	templateID := os.Getenv("TEMPLATE_ID")
+	fromEmail := os.Getenv("FROM_EMAIL")
+	csvFile := os.Getenv("CSV_FILE")
+	emailSubject := os.Getenv("EMAIL_SUBJECT")
+	emailContent := os.Getenv("EMAIL_CONTENT")
+
+	if sendgridAPIKey == "" || templateID == "" || fromEmail == "" || csvFile == "" || emailSubject == "" || emailContent == "" {
+		log.Fatalf("Environment variables not set correctly")
+	}
+
+	file, err := os.Open(csvFile)
+	if err != nil {
+		log.Fatalf("Failed to open CSV file: %s", err)
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	headers, err := reader.Read()
+	if err != nil {
+		log.Fatalf("Failed to read CSV headers: %s", err)
+	}
+
+	for {
+		record, err := reader.Read()
+		if err != nil {
+			break
+		}
+
+		dynamicData := make(map[string]string)
+		for i, header := range headers {
+			dynamicData[header] = record[i]
+		}
+
+		toEmail := dynamicData["email"]
+		if err := sendEmail(sendgridAPIKey, templateID, fromEmail, toEmail, dynamicData, emailSubject, emailContent); err != nil {
+			log.Printf("Failed to send email to %s: %s", toEmail, err)
+		}
+	}
+}
+
+func sendEmail(apiKey, templateID, fromEmail, toEmail string, dynamicData map[string]string, subject, content string) error {
+	from := mail.NewEmail("Example User", fromEmail)
+	to := mail.NewEmail("Recipient", toEmail)
+	message := mail.NewSingleEmail(from, subject, to, content, "")
+	message.SetTemplateID(templateID)
+	for key, value := range dynamicData {
+		message.Personalizations[0].SetDynamicTemplateData(key, value)
+	}
+
+	client := sendgrid.NewSendClient(apiKey)
+	response, err := client.Send(message)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("Email sent to %s with status code %d\n", toEmail, response.StatusCode)
+	return nil
+}
